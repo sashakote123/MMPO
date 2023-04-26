0b42f6a772c6b50f7e232e72537b3cd1de7248fc
Sasha <sapool@bk.ru>
2023-04-27 00:45:29 +0300

    v3

--- graph.py
+++ graph.py
@@ -2,49 +2,39 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
-n = 5  # число вершин
-m = 2  # средняя степень вершин
+import numpy as np
 
-# создаем начальный граф из m вершин
-G = nx.complete_graph(m)
 
-# добавляем новые вершины
-for i in range(m, n):
-    # выбираем m вершин из существующих
-    targets = []
-    while len(targets) < m:
-        # выбираем случайную вершину
-        node = random.choice(list(G.nodes()))
-        # проверяем, не добавляли ли мы уже эту вершину
-        if node not in targets:
-            targets.append(node)
-    # добавляем новую вершину и соединяем ее с выбранными вершинами
-    G.add_node(i)
-    for target in targets:
-        G.add_edge(i, target)
+def generateGraph(n, m):
+    # создаем начальный граф из m вершин
+    G = nx.complete_graph(m)
 
-subax1 = plt.plot()
-nx.draw(G, with_labels=True, node_color='green')
-print(nx.edges(G))
+    # добавляем новые вершины
+    for i in range(m, n):
+        # выбираем m вершин из существующих
+        targets = []
+        while len(targets) < m:
+            # выбираем случайную вершину
+            node = random.choice(list(G.nodes()))
+            # проверяем, не добавляли ли мы уже эту вершину
+            if node not in targets:
+                targets.append(node)
+        # добавляем новую вершину и соединяем ее с выбранными вершинами
+        G.add_node(i)
+        for target in targets:
+            G.add_edge(i, target)
 
-for i in range(n):
-    print(nx.edges(G, i))
-    print(len(nx.edges(G, i)))
+        for i in range(len(G.nodes)):
+            G.nodes[i]['p'] = random.random()
+            G.nodes[i]['betta'] = random.random()
+            G.nodes[i]['gamma'] = random.random()
+            G.nodes[i]['isSick'] = False
+            for j in G[i]:
+                G[i][j]['w'] = random.random()
 
-T = 2
-N = n
-omega = 0.5
-p_list = []
-beta_list = []
-gamma_list = []
-nodes_list = []
+    return G
 
-for i in range(N):
-    p_list.append(0.2)
-    beta_list.append(random.random())
-    gamma_list.append(random.random())
-    nodes_list.append(len(nx.edges(G, i)))
-
+#def generateNewGraph():
 
 def external_probability(p_list, omega, beta_list, nodes_list):
     N = len(p_list)
@@ -57,45 +47,42 @@ def external_probability(p_list, omega, beta_list, nodes_list):
     return p_vnehn_list
 
 
-p1_list = []
-for i in range(N):
-    p1_list.append((1 - p_list[i]) * p_vnehn_list[i] - gamma_list[i] * p_list[i] + p_list[i])
+def GetNewProbability(G):
+    p_list = []
+    a = 0
+    for i in range(len(G.nodes)):
+        for j in G[i]:
+            a += G[i][j]['w'] * G.nodes[j]['betta'] * G.nodes[j]['p']
+        p_list.append((1 - G.nodes[i]['p']) * a - G.nodes[i]['gamma'] * G.nodes[i]['p'] + G.nodes[i]['p'])
+        if p_list[i] < 0:
+            p_list[i] = 0
 
-print(p_vnehn_list)
-print(p1_list)
+        if p_list[i] > 1:
+            p_list[i] = 1
+        a = 0
 
-for i in range(N):
-    p_list[i] = p1_list[i]
+    for i in range(len(G.nodes)):
+        G.nodes[i]['p'] = p_list[i]
+        G.nodes[i]['isSick'] = random.choices([True, False], weights=[G.nodes[i]['p'], 1 - G.nodes[i]['p']])[0]
+    return G
 
-p_vnehn_list = []
-for j in range(N):
-    p_tmp = 0
-    for i in range(nodes_list[j]):
-        p_tmp += omega * beta_list[j] * p_list[j]
-    p_vnehn_list.append(p_tmp)
 
-p1_list = []
-for i in range(N):
-    p1_list.append((1 - p_list[i]) * p_vnehn_list[i] - gamma_list[i] * p_list[i] + p_list[i])
+def run(G, t):
+    T = np.arange(1, t, 1)
+    for j in T:
+        print(f'День №{j + 1}****************************')
+        G = GetNewProbability(G)
 
-print(p_vnehn_list)
-print(p1_list)
+        for i in range(len(G.nodes)):
+            print(f"Нода номер: {i}, p =  {G.nodes[i]['p']}, Статус: {G.nodes[i]['isSick']}")
+            print('\n')
 
-for i in range(N):
-    p_list[i] = p1_list[i]
 
-p_vnehn_list = []
-for j in range(N):
-    p_tmp = 0
-    for i in range(nodes_list[j]):
-        p_tmp += omega * beta_list[j] * p_list[j]
-    p_vnehn_list.append(p_tmp)
+G = generateGraph(10, 3)
+run(G, 100)
 
-p1_list = []
-for i in range(N):
-    p1_list.append((1 - p_list[i]) * p_vnehn_list[i] - gamma_list[i] * p_list[i] + p_list[i])
+subax1 = plt.plot()
+nx.draw(G, with_labels=True, node_color='green')
+print(nx.edges(G))
 
-print(p_vnehn_list)
-print(p1_list)
-
-plt.show()
+#plt.show()
